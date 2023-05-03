7ff82e9a3938db7af831661c66bf1dbd17b5ca50
MatteoGunetto <Matteogunetto@tiscali.it>
2023-05-03 15:19:38 +0200

    parte logica terminata

--- dadi/javaScriptDadi.js
+++ dadi/javaScriptDadi.js
@@ -1,3 +1,50 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+
+// settiamo le variabili
+const contenitore = document.getElementById("contenitore");
+let utente, computer, vicintore;
+let risultato =document.createElement("h1");
+
+// creiamo due generatori di numeri random 
+
+// generatore numeri random computer 
+computer = Math.floor(Math.random() *  (6) +1) ;
+console.log(computer);
+
+// generatore numeri random utente 
+utente = Math.floor(Math.random() *  (6) +1);
+console.log(utente);
+
+// confrontiamo i due numneri generati
+
+// vicitore utente
+if (utente > computer){
+ 
+    // console.log('Utente ha vinto!')
+    vincitore = "Utente ha vinto!"
+}
+
+// vincitore computer
+else if (utente < computer){
+    // console.log('Computer ha vinto!')
+    vincitore="Computer ha vinto!"
+}
+// pareggio
+
+else{
+    // console.log('Pareggio!')
+    vincitore="Pareggio"
+
+}
+console.log(vincitore)
+risultato.append(vincitore)
+
+contenitore.append(risultato)
+
+
+
+
--- mail/javaScriptMail.js
+++ mail/javaScriptMail.js
@@ -1,3 +1,37 @@
 // Mail
 // Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 // QUI ricordatevi che non è concesso usare altro oltre quello che abbiam visto oggi 
+
+
+// creaimao una lista mail  
+const listMail = [ 'matteogun@gmail.com', 'pallopinco@gmail.com', 'push@live.it', 'Rsara@tiscali.it', 'pippo62@libero.it' ];
+console.log(listMail);
+let messaggio =document.createElement("h1");
+
+
+// chiediamo ad un utente di inserire la propria mail
+const newMail = prompt('Inserisci la tua mail: ');
+let AcceptMail = 0;
+
+
+// confrontiamo SE la mail inserita è presente nella lista di partenza 
+for ( let i = 0; i <= listMail.length; i++)
+{
+    if( newMail == listMail[i])
+    {
+        AcceptMail = 1;
+    }
+}
+// SE è presente consentiamo l'accesso all'utente 
+if( AcceptMail == 0){
+    AcceptMail='Email non autorizzata';
+}
+// ALTRIMENTI neghiamo l'accesso all'utente 
+else{
+    AcceptMail='Email autorizzata';
+}
+
+messaggio.append(AcceptMail)
+console.log(AcceptMail);
+
+contenitore.append(messaggio)
